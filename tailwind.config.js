ac67a2891cd6f0c49930b68710e9d815f1e401bd
Leunerd <samadeol03@gmail.com>
2023-06-04 02:18:49 +0100

    First Tesla clone commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+    './Tailwind/**/*.{html,js,css}'
+  ],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
